99d06ab85a2f00ced986ae96b57928d8dcfd7ab2
chromers <digitalartcreative@hotmail.com>
2021-05-24 12:28:30 +0700

    simple startup website template themes download gratis free

---
+++ readme.md
@@ -0,0 +1,92 @@
+# New Modern CMS Build a website without database.
+---
+A new generation get simpe cms axcora tech for develope website project powerfull !!
+
+No need a database for installation , make your website very fast and SEO friendly.
+
+Simple startup themes template website apps. Modern design with boostrap 5 + fontawesome
+--
+![free website template simple start up gratis](https://a.fsdn.com/con/app/proj/aplikasi-toko/screenshots/free%20website%20template%20themes%20download%20gratis%20%281%29.png/max/max/1)
+
+Home page
+
+![free website template simple start up gratis](https://a.fsdn.com/con/app/proj/aplikasi-toko/screenshots/free%20website%20template%20themes%20download%20gratis%20%284%29.png/max/max/1)
+
+Pricing services page
+
+![free website template simple start up gratis](https://a.fsdn.com/con/app/proj/aplikasi-toko/screenshots/free%20website%20template%20themes%20download%20gratis%20%285%29.png/max/max/1)
+
+Other page
+
+![free website template simple start up gratis](https://a.fsdn.com/con/app/proj/aplikasi-toko/screenshots/free%20website%20template%20themes%20download%20gratis%20%283%29.png/max/max/1)
+
+Contact Page
+
+------
+
+Login Area for backend
+
+![new modern website cms](https://a.fsdn.com/con/app/proj/getaxcoracms/screenshots/New%20CMS%20modern%20website%20SEO%20%285%29.png/max/max/1)
+
+Page and article post menu.
+
+![new modern website cms](https://a.fsdn.com/con/app/proj/getaxcoracms/screenshots/New%20CMS%20modern%20website%20SEO%20%284%29.png/max/max/1)
+
+Create article post very simple and easy
+
+![new modern website cms](https://a.fsdn.com/con/app/proj/getaxcoracms/screenshots/New%20CMS%20modern%20website%20SEO%20%282%29.png/max/max/1)
+
+Back end setting
+
+ -----------------------------------------------------------------
+### how to install
+
+[read documentation →](https://axcora.com/getaxcoracms/index.php?id=get-started)
+
+CLOUDS / SHARED HOSTING
+
+Just download this file and upload on your shared or clouds host , upload on public_html fle if you using this cms for develope home page website, or you can upload it on sub directori .
+After upload progress success, visit your website url/admin then login with username : axcora / pass : axcoracms
+open on setting menu and change url with your website url , change password , then save it, and your website is online now...
+
+LOCALHOST
+
+Need third party XAMPP WAMPP LAMPP server, download this project and upload on your xampp/htdocs folder, then visit localhost/your project/admin.
+then login with username : axcora / pass : axcoracms
+open on setting menu and change url with your website url , change password , then save it, and your website is online now...
+
+---
+Features :
+
+Frontend
++ Home page design with landing page
++ About page
++ Info page 
++ Profile page
++ Gallery Page
++ Contact Page information
+---
+Backend
++ Page for create new post article content you can edit and delete too.
++ File menu for upload media manager
++ Setting for change password and others.
++ SEO : automaticly generate meta title , description , facebook, twitter.
+----
+
+No need a database installation make your website very fast, and of course with SEO friendly include, simple and easy to use.
+
+----
+
+Need demo ?? 
+
+[Demo 1 →](https://resto-x.sourceforge.io/)
+
+[Demo 2 →](https://aplikasitoko-online-webapps.sourceforge.io/)
+
+
+More themes :
+https://axcora.com/getaxcoracms
+
+present by https://axcora.com
+
+Hapy codding !!
---
+++ theme/axcora/header.inc.php
@@ -0,0 +1,33 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); } ?><!doctype html><html lang="en" ><head><meta charset="utf-8"><title><?php get_page_clean_title(); ?> - <?php get_site_name(); ?></title><meta content='<?php get_page_title(); ?>' property='og:title'/><meta content='<?php get_page_clean_title(); ?> - <?php get_site_name(); ?>' property='og:description'/><meta content='https://axcora.com' property='og:author'/><meta content='<?php get_page_title(); ?>' name='facebook:title'/><meta content='website' property='og:type'/><meta content='https://www.facebook.com/mesinkasircomplete' property='facebook:author'/><meta content='https://bstp.sourceforge.io/cov.jpg' name='facebook:image'/><meta content='<?php get_page_title(); ?>' property='og:site_name'/><meta content='<?php get_page_clean_title(); ?> - <?php get_site_name(); ?>' property='facebook:description'/><meta content='summary_large_image' name='twitter:card'/><meta content='https://twitter.com/hockeycomp' property='twitter:author'/><meta content='@hockeycomp' name='twitter:site'/><meta content='https://bstp.sourceforge.io/cov.jpg' name='twitter:image'/><meta content='<?php get_page_clean_title(); ?> - <?php get_site_name(); ?>' name='twitter:description'/><meta content='<?php get_page_title(); ?> ' name='twitter:title'/><meta content='<?php get_page_title(); ?>' name='keywords'/><meta content='index, follow' name='robots'/><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" /> <script crossorigin="anonymous" src="https://kit.fontawesome.com/42d5adcbca.js"></script> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"><meta name="robots" content="index, follow"><link rel="icon" type="image/x-icon" href="https://axcora.com/img/angular.png" /><link rel="author" href="https://axcora.com" /></head><body id="<?php get_page_slug(); ?>" ><div class="container">
+	<nav class="navbar navbar-expand-lg bg-warning fixed-top p-3">
+		<div class="container"> <strong><a class="navbar-brand text-dark" href="index.php">
+		<i class="fab fa-angular fa-2x"></i>
+		<!--<?php get_site_name(); ?>--></a></strong> 
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" 
+			aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-ellipsis-v text-dark"></i>
+			</button>
+			<div class="collapse navbar-collapse justify-content-end p-1 text-center" id="navbarNavAltMarkup">
+				<ul class="navbar-nav">
+					<li class="nav-item"> <a class="nav-link text-dark" href="index.php"><i class="fas fa-home"></i> Home</a>
+					</li>                                                 
+					<li class="nav-item"> <a class="nav-link text-dark" href="index.php?id=about"><i class="fas fa-info-circle"></i> About</a>
+					</li>                                
+					<li class="nav-item"> <a class="nav-link text-dark" href="index.php?id=fitur-apps"><i class="fas fa-cash-register"></i> Kasir App</a>
+					</li>                       
+					<li class="nav-item"> <a class="nav-link text-dark" href="index.php?id=website-online-shop"><i class="fab fa-chrome"></i> Website</a>
+					</li>                                    
+					<li class="nav-item"> <a class="nav-link text-dark" href="index.php?id=android-app"><i class="fab fa-android"></i> Android</a>
+					</li>                                       
+					<li class="nav-item"> <a class="nav-link text-dark" href="index.php?id=pricing"><i class="fas fa-rocket"></i> Clouds</a>
+					</li>                                   
+					<li class="nav-item"> <a class="nav-link text-dark" href="index.php?id=contact"><i class="fas fa-headphones"></i> Contact</a>
+					</li>                              
+					<li class="nav-item text-center "> <a class="nav-link bg-dark text-white rounded" href="https://mesinkasir.github.io/e-catalog/dexopos%20shop.pdf"><i class="fas fa-arrow-alt-circle-down"></i> Download</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+<div class="row">
+<div class="col-12 col-md-12 p-3 p-md-5">
